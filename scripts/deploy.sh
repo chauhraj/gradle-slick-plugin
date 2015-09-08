4040fb29e29a6d62ba77fd1d168e654c23cd1716
Rajiv Chauhan <rajiv.chauhan@gmail.com>
2015-09-08 04:42:40 -0400

    Adding debug statements to track Travis deployment failure

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,5 +9,11 @@ else
 fi
 echo "gradle.publish.key=${GRADLE_PUBLISH_KEY}" >> ${GRADLE_PROPERTIES_FILE}
 echo "gradle.publish.secret=${GRADLE_PUBLISH_SECRET}" >> ${GRADLE_PROPERTIES_FILE}
-./gradlew -Prelease.disableChecks -Prelease.attachRemote=ssh://git@github.com:chauhraj/gradle-slick-plugin.git release publishPlugins
+./gradlew -Prelease.disableChecks \
+          -Prelease.attachRemote=ssh://git@github.com:chauhraj/gradle-slick-plugin.git \
+          -Pgradle.publish.key=${GRADLE_PUBLISH_KEY}
+          -Pgradle.publish.secret=${GRADLE_PUBLISH_SECRET}
+          --info -S
+          release \
+          publishPlugins
 
