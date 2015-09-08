429c98d8c9661c2ca70fad26772ccdb74a50cb45
Rajiv Chauhan <rajiv.chauhan@gmail.com>
2015-09-08 09:08:18 -0400

    Hopefully travis build would work

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,10 +7,6 @@ echo "gradle.publish.key=${GRADLE_PUBLISH_KEY}" >> ${GRADLE_PROPERTIES_FILE}
 echo "gradle.publish.secret=${GRADLE_PUBLISH_SECRET}" >> ${GRADLE_PROPERTIES_FILE}
 
 GRADLE_OPTS="-Prelease.disableChecks"
-GRADLE_OPTS="${GRADLE_OPTS} -Prelease.attachRemote=ssh://git@github.com:chauhraj/gradle-slick-plugin.git" 
-GRADLE_OPTS="${GRADLE_OPTS} -Pgradle.publish.key=${GRADLE_PUBLISH_KEY}"
-GRADLE_OPTS="${GRADLE_OPTS} -Pgradle.publish.secret=${GRADLE_PUBLISH_SECRET}"
-GRADLE_OPTS="${GRADLE_OPTS} -Pgradle.publish.secret=${GRADLE_PUBLISH_SECRET}"
-GRADLE_OPTS="${GRADLE_OPTS} -Prelease.customUsername=chauhraj"
-GRADLE_OPTS="${GRADLE_OPTS} -Prelease.customPassword='automatic releases for chauhraj/gradle-slick-plugin'"
+GRADLE_OPTS="${GRADLE_OPTS} -Prelease.customUsername=${GITHUB_ACCESS_TOKEN}"
+
 ./gradlew  ${GRADLE_OPTS} --info -S release publishPlugins
